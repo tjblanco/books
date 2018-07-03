82bb8f9a68c7e268e31caa48074ea6bb66f80081
Teresa Juan Blanco <teresa.juanblanco@genomcore.com>
2018-07-03 17:55:54 +0200

    Fix search

--- src/Search.jsx
+++ src/Search.jsx
@@ -9,7 +9,7 @@ class Search extends Component {
     }
 
     updateQuery = (query) => {
-        if(query){
+        if(query.length){
             this.setState({query})
             BooksAPI.search(query).then((books) => {
                 if(books.length){
@@ -53,7 +53,12 @@ class Search extends Component {
                             <li>
                                 <div className="book">
                                     <div className="book-top">
-                                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.url})` }}></div>
+                                        {book.imageLinks ?
+                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div> :
+
+                                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(http://via.placeholder.com/128x193?text=?)'}}></div>
+                                        }
+
                                         <div className="book-shelf-changer">
                                             <select>
                                                 <option value="move" disabled>Move to...</option>
